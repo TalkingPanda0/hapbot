4c07e2649078143764f794468f7fb5c186c45151
TalkingPanda0 <69118613+TalkingPanda0@users.noreply.github.com>
2023-01-10 16:54:57 +0300

    Update hapbot.js

--- hapbot.js
+++ hapbot.js
@@ -4,8 +4,10 @@
 	var hapbo = 0;
 	var siid;
 	function setTitle() {
-		title = "HapBoo has been said in the chat " + hapbo + " times!";
+		title = "HapBoo has been sent in the chat " + hapbo + " times!";
+		title = title.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 		$.updateStatus($.channelName, title , "hapbot" ,1);
+		$.say(title);
 	}
 	$.bind('ircModeration', function (event) {
 	    hapbo += event.getMessage().match(/sweetb35HapBoo/g).length
